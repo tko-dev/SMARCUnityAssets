a9acd0f6586651ba84367f614bb018e82e2c1a96
Mart Kartašev <mart.kartasev@gmail.com>
2023-11-20 11:58:16 +0100

    Refactored propeller

--- Runtime/Scripts/Force/SAMUnityForceModel.cs
+++ Runtime/Scripts/Force/SAMUnityForceModel.cs
@@ -55,12 +55,23 @@ namespace DefaultNamespace
             var r1 = rpm1 / 1000;
             var r2 = rpm2 / 1000;
 
-            var localDirection = Quaternion.Euler(d_aileron * Mathf.Rad2Deg , -d_rudder * Mathf.Rad2Deg, 0) * transform.InverseTransformDirection(transform.forward);
-            var localScaled = localDirection * (float)r1 * rpm_multiplier;
-            var globalDirection = transform.TransformDirection(localScaled);
-            rigidBody.AddForceAtPosition(globalDirection, transform.position + transform.TransformDirection(new Vector3(0, 0, -0.73f)), ForceMode.Force);
+
+            var rotorPositionGlobalFrame = transform.position + transform.TransformDirection(new Vector3(0, 0, -0.73f));
+
+            rigidBody.AddForceAtPosition(ThrustVectorForPropller(r1), rotorPositionGlobalFrame, ForceMode.Force);
+            rigidBody.AddForceAtPosition(ThrustVectorForPropller(r2), rotorPositionGlobalFrame, ForceMode.Force);
+            rigidBody.AddTorque(Vector3.forward * -(float)r1 * rpm_multiplier / 2, ForceMode.Force);
+            rigidBody.AddTorque(Vector3.forward * (float)r2 * rpm_multiplier / 2, ForceMode.Force);
         }
 
+        public Vector3 ThrustVectorForPropller(double r)
+        {
+            var localDirection = Quaternion.Euler(d_aileron * Mathf.Rad2Deg, -d_rudder * Mathf.Rad2Deg, 0) * transform.InverseTransformDirection(transform.forward);
+            var localScaled = localDirection * (float)r * rpm_multiplier / 2; //Divide by 2 since two forces
+            var globalDirection = transform.TransformDirection(localScaled);
+            return globalDirection;
+        }
+        
         public Vector3 GetTorqueDamping()
         {
             throw new System.NotImplementedException();
