721b7bd6414d00b5295ba10d41623eded3f86c4d
martkartasev <mart.kartasev@gmail.com>
2024-05-02 16:20:12 +0200

    Updated assets to conform with smarc repo (#5)
    * WIP
    
    * Sam rigidbody shape
    
    * Angle of attack impl
    
    * Damping calc reimpl NED
    
    * DamÃping force in wrong direction
    
    * Status
    
    * algae cloth
    
    * Fix standard assets
    
    * playing with sam config
    
    * Volume calculation and other stuff
    
    * better sam defaults
    
    * Good defaults
    
    * WIP Fixing bouys
    
    * Better fixed points and angular drag
    
    * SAM Prefab defaults
    
    * proper normalized force interface
    
    * Normalized interface in sam prefab
    
    * Forcepoint dont recompute known volume
    
    * Sam prefab with volume calc
    
    * Change Unity force model back to true inputs and add normalization layer
    
    * Fix keyboard control
    
    * Correct co-ordinate transforms
    
    * Progress, but values are wrong
    
    * Fixed update
    
    * Reenable sensors
    
    * Sync (#2)
    
    * Mart's changes (#3)
    
    * WIP
    
    * Sam rigidbody shape
    
    * Angle of attack impl
    
    * Damping calc reimpl NED
    
    * DamÃping force in wrong direction
    
    * Status
    
    * algae cloth
    
    * Fix standard assets
    
    * playing with sam config
    
    * Volume calculation and other stuff
    
    * better sam defaults
    
    * Good defaults
    
    * WIP Fixing bouys
    
    * Better fixed points and angular drag
    
    * SAM Prefab defaults
    
    * proper normalized force interface
    
    * Normalized interface in sam prefab
    
    * Forcepoint dont recompute known volume
    
    * Sam prefab with volume calc
    
    * Change Unity force model back to true inputs and add normalization layer
    
    * Fix keyboard control
    
    * Correct co-ordinate transforms
    
    * Progress, but values are wrong
    
    * Fixed update
    
    * Reenable sensors
    
    * Revert "Mart's changes (#3)" (#4)
    
    This reverts commit 5f814294847483b8ce264bf786b8d49a4fe1b1a1.
    
    ---------
    
    Co-authored-by: Özer Özkahraman <KKalem@users.noreply.github.com>
    
    * reasonable defaults
    
    * Fixes
    
    ---------
    
    Co-authored-by: Özer Özkahraman <KKalem@users.noreply.github.com>

---
+++ Runtime/Scripts/Force/LookUpTable/LookUpTables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.Plastic.Newtonsoft.Json;
+using UnityEngine;
+
+namespace Force.LookUpTable
+{
+    [Serializable]
+    public class LookUpTables
+    {
+        public Double[][] xcp;
+        public Double[][] cz;
+        public Double[][] cy;
+        public Double[][] cx;
+
+        public static LookUpTables CreateFromJSON(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<LookUpTables>(jsonString);
+        }
+    }
+}
