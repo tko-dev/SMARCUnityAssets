742cf0976deb36830b14f893f5cfcaf038b07fde
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2024-05-14 15:02:01 +0200

    Remove ros requirement of keyboard controller

--- Runtime/Scripts/SAM/SAMKeyboardControl.cs
+++ Runtime/Scripts/SAM/SAMKeyboardControl.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 namespace DefaultNamespace
 {
     [RequireComponent(typeof(ISAMControl))]
-    [RequireComponent(typeof(SamActuatorController))]
     public class SAMKeyboardControl : MonoBehaviour
     {
         private ISAMControl _samControl;
-
-        private SamActuatorController rosControl;
-
-        [Tooltip("If true, pressing any key will disable the ros controllers")]
-        public bool takeOverRosController = true;
 
         public float rollRpms = 0.1f;
         public float moveRpms = 800f;
@@ -27,7 +21,6 @@ namespace DefaultNamespace
         private void Awake()
         {
             _samControl = GetComponentInParent<ISAMControl>();
-            rosControl = GetComponent<SamActuatorController>();
         }
 
         private void Update()
@@ -40,85 +33,71 @@ namespace DefaultNamespace
             if (Input.GetKeyDown("down"))
             {
                 _samControl.SetRpm(-moveRpms, -moveRpms);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("q"))
             {
                 _samControl.SetRpm(-rollRpms, rollRpms);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("e"))
             {
                 _samControl.SetRpm(rollRpms, -rollRpms);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("up"))
             {
                 _samControl.SetRpm(moveRpms, moveRpms);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyUp("up") || Input.GetKeyUp("down") || Input.GetKeyUp("q") || Input.GetKeyUp("e"))
             {
                 _samControl.SetRpm(0, 0);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("a"))
             {
                 _samControl.SetRudderAngle(-1);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("d"))
             {
                 _samControl.SetRudderAngle(1);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
             {
                 _samControl.SetRudderAngle(0);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("w"))
             {
                 _samControl.SetElevatorAngle(-1);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("s"))
             {
                 _samControl.SetElevatorAngle(1);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyUp("w") || Input.GetKeyUp("s"))
             {
                 _samControl.SetElevatorAngle(0);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("f"))
             {
                 _samControl.SetWaterPump(0.5f);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("c"))
             {
                 _samControl.SetWaterPump(0);
-                if (takeOverRosController) rosControl.enable = false;
             }
 
             if (Input.GetKeyDown("space"))
             {
                 _samControl.SetWaterPump(1);
-                if (takeOverRosController) rosControl.enable = false;
             }
         }
     }
