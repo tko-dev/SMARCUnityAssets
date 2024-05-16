5a7f2b8fcc6b3034273771169d98c418aa130ae9
Özer Özkahraman <KKalem@users.noreply.github.com>
2024-05-16 11:13:44 +0200

    Currents~ (#6)
    * Added a prefix option to the TF publisher, no more renaming stuff!
    
    * Started the object agnostic sensor/actuator setup
    
    * More sensors
    
    * Links can now be rotated, SSS, Sonar
    
    * Added hasData to sensors for ros-publishing timing. Sonar is now Sensor too
    
    * Move gizmos to link attachment
    
    * Added ros publisher, generic af
    
    * Added camera stuff, samsensor prefab
    
    * Sensors done, needs validation+TF
    
    * Fixed SSS head alignment
    
    * Moved the core ROS stuff into the new setup. TF is now a simple sensor with a simple prefix option.
    
    * Removed old TF stuff. Modified force and prefabs to use articulation body instead of rigid body
    
    * Removed old stuff, fixed clock, organized sensors prefab
    
    * Moved ros messages to vehicle_components/ros/core/
    
    * Leakmsg sam->smarc. sensors prefab
    
    * Added the actuator feedback publishers as sensors to sam sensors prefab
    
    * Added sam thrust control subs
    
    * Added individual rpm controls to sam force model
    
    * Added VBS, LCG subs, with a todo
    
    * Remove ros requirement of keyboard controller
    
    * Some prefab organization and propeller prefab
    
    * Added parent arti body requisition to link attachment
    
    * All thrust are now in its own objects
    
    * Keyboard controls working again.
    
    * Added simple water currents. Uses forcepoints

---
+++ Runtime/Scripts/Water/SimpleWaterCurrent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Force; // for force points
+
+namespace DefaultNamespace.Water
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class SimpleWaterCurrent : MonoBehaviour, IWaterCurrent
+    {        
+        public Vector3 current = new Vector3(0,0,0);
+        Collider col;
+
+        void Awake()
+        {
+            col = GetComponent<Collider>();
+        }
+
+        public Vector3 GetCurrentAt(Vector3 position)
+        {
+            return current;
+        }
+
+        void OnDrawGizmos()
+        {
+            
+            BoxCollider col = GetComponent<BoxCollider>();
+            Vector3 size = col.bounds.size;
+            // Random numbers. 5 seems ok for inside of a box.
+            // Actually getting the exact volume of an arbitrary box from
+            // a collider seems like a PITA, so i shant.
+            double sizeDiv = 5;
+            double spacing = 1;
+            for(double x = -(size.x)/sizeDiv; x < (size.x)/sizeDiv; x += spacing)
+                for(double y = -(size.y)/sizeDiv; y < (size.y)/sizeDiv; y += spacing)
+                    for(double z = -(size.z)/sizeDiv; z < (size.z)/sizeDiv; z += spacing)
+                    {
+                        Vector3 p = new Vector3((float)x,(float)y,(float)z);
+                        Vector3 c = GetCurrentAt(p);
+                        Gizmos.color = new Color(c.x, c.y, c.z, 1f);
+                        Gizmos.DrawRay(transform.position + p, c);
+                        Gizmos.color = new Color(c.x, c.y, c.z, 0.1f);
+                        Gizmos.DrawSphere(transform.position + p, 0.1f);
+                    }
+                        
+
+            Gizmos.color = new Color(1f, 0, 1f, 0.05f);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            
+
+        }
+
+    }
+}
