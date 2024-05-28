85d9dfb540bfa84e04cb5ca275560085984e9133
Mart Kartasev <mart.kartasev@gmail.com>
2024-05-28 08:36:32 +0300

    RPM Inputs

--- Runtime/Scripts/Force/BlueROV2ForceModel.cs
+++ Runtime/Scripts/Force/BlueROV2ForceModel.cs
@@ -17,6 +17,15 @@ namespace DefaultNamespace
         public ArticulationBody propeller_back_left_bottom;
         public ArticulationBody propeller_back_right_bottom;
 
+        public float rpm_front_left_top = 0.0f;
+        public float rpm_front_right_top = 0.0f;
+        public float rpm_back_left_top = 0.0f;
+        public float rpm_back_right_top = 0.0f;
+        public float rpm_front_left_bottom = 0.0f;
+        public float rpm_front_right_bottom = 0.0f;
+        public float rpm_back_left_bottom = 0.0f;
+        public float rpm_back_right_bottom = 0.0f;
+
         public void Start()
         {
             mainBody.inertiaTensor = Vector3.one; //Can set inertia hera
@@ -28,32 +37,35 @@ namespace DefaultNamespace
             var mainBodyVelocity = mainBody.velocity;
             var mainBodyAngularVelocity = mainBody.angularVelocity;
             var mainBodyMass = mainBody.mass;
-            
+
 
             var inverseTransformDirection_local = transform.InverseTransformDirection(mainBody.velocity);
             var transformAngularVelocity_local = transform.InverseTransformDirection(mainBody.angularVelocity);
-            
+
             //An additional transform. From Unity RUF to a more appropriate frame of reference.
-            var velocity_CorrectCoordinateFrame = inverseTransformDirection_local.To<NED>().ToDense(); // Might need to revisit. Rel. velocity in point m block.
-            var angularVelocity_CorrectCoordinateFrame = FRD.ConvertAngularVelocityFromRUF(transformAngularVelocity_local).ToDense();
+            var velocity_CorrectCoordinateFrame =
+                inverseTransformDirection_local.To<NED>()
+                    .ToDense(); // Might need to revisit. Rel. velocity in point m block.
+            var angularVelocity_CorrectCoordinateFrame =
+                FRD.ConvertAngularVelocityFromRUF(transformAngularVelocity_local).ToDense();
 
             // Do calculations here
-            
+
             // Resulting force and torque vectors
 
             var resultingForce = Vector3.zero;
             var resultingTorque = Vector3.zero;
-            
+
             mainBody.AddRelativeForce(resultingForce);
             mainBody.AddRelativeTorque(resultingTorque);
-            
+
             // Set RPMs for Visuals
-            propeller_front_left_top.SetDriveTargetVelocity(ArticulationDriveAxis.X, 0); 
+            propeller_front_left_top.SetDriveTargetVelocity(ArticulationDriveAxis.X, 0);
             propeller_front_right_top.SetDriveTargetVelocity(ArticulationDriveAxis.X, 0);
             propeller_back_left_top.SetDriveTargetVelocity(ArticulationDriveAxis.X, 0);
             propeller_back_right_top.SetDriveTargetVelocity(ArticulationDriveAxis.X, 0);
-            
-            propeller_front_left_bottom.SetDriveTargetVelocity(ArticulationDriveAxis.Z, 0); 
+
+            propeller_front_left_bottom.SetDriveTargetVelocity(ArticulationDriveAxis.Z, 0);
             propeller_front_right_bottom.SetDriveTargetVelocity(ArticulationDriveAxis.Z, 0);
             propeller_back_left_bottom.SetDriveTargetVelocity(ArticulationDriveAxis.Z, 0);
             propeller_back_right_bottom.SetDriveTargetVelocity(ArticulationDriveAxis.Z, 0);
