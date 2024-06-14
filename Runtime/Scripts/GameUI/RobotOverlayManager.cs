025d008bb73cc9784768b0194a7fcfa398b14fce
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2024-06-14 17:06:11 +0200

    Made gameGUI a prefab. Basic, but functional for now

--- Runtime/Scripts/GameUI/RobotOverlayManager.cs
+++ Runtime/Scripts/GameUI/RobotOverlayManager.cs
@@ -10,65 +10,28 @@ namespace GameUI
 {
     public class RobotOverlayManager : MonoBehaviour
     {
-        GameObject[] robots;
-        GameObject[] baseLinks;
-        GameObject[] robotMarkers;
         Canvas canvas;
         CameraManager cameraManager;
+        [Tooltip("Assign a prefab to be spawned on screen")]
+        public GameObject OverlayPrefab;
 
-        public GameObject RobotMarker;
+        
 
-        // https://discussions.unity.com/t/how-to-convert-from-world-space-to-canvas-space/117981/18
-        private Vector2 WorldToCanvasPosition(Canvas canvas, Camera worldCamera, Vector3 worldPosition) 
+        void Awake()
         {
-            //Vector position (percentage from 0 to 1) considering camera size.
-            //For example (0,0) is lower left, middle is (0.5,0.5)
-            Vector2 viewportPoint = worldCamera.WorldToViewportPoint(worldPosition);
-
-            var rootCanvasTransform = (canvas.isRootCanvas ? canvas.transform : canvas.rootCanvas.transform) as RectTransform;
-            var rootCanvasSize = rootCanvasTransform!.rect.size;
-            //Calculate position considering our percentage, using our canvas size
-            //So if canvas size is (1100,500), and percentage is (0.5,0.5), current value will be (550,250)
-            var rootCoord = (viewportPoint - rootCanvasTransform.pivot) * rootCanvasSize;
-            if (canvas.isRootCanvas)
-                return rootCoord;
-
-            var rootToWorldPos = rootCanvasTransform.TransformPoint(rootCoord);
-            return canvas.transform.InverseTransformPoint(rootToWorldPos);
-        }
-
-        void Start()
-        {
-            robots = GameObject.FindGameObjectsWithTag("robot");
-            robotMarkers = new GameObject[robots.Length];
-            baseLinks = new GameObject[robots.Length];
+            var robots = GameObject.FindGameObjectsWithTag("robot");
+            canvas = GetComponent<Canvas>();
+            cameraManager = FindObjectsByType<CameraManager>(FindObjectsSortMode.None)[0];
             for(int i=0; i<robots.Length; i++)
             {
                 var robot = robots[i];
 
-                // TODO Possibly instantiate a prefab with a script in it that allows
-                // easy setting of texts, values etc. 
-                var textGO = Instantiate(RobotMarker);
-                textGO.transform.SetParent(transform);
-                textGO.name = $"Text_{robot.transform.root.name}";
-                var text = textGO.GetComponent<TMP_Text>();
-                text.SetText(robot.transform.root.name);
-
-                robotMarkers[i] = textGO;
-                baseLinks[i] = Utils.FindDeepChildWithName(robot, "base_link");
+                var overlayGO = Instantiate(OverlayPrefab);
+                var overlay = overlayGO.GetComponent<RobotOverlay>();
+                overlay.Initialize(robot, canvas, cameraManager);
+                overlayGO.transform.SetParent(transform);
             }
-            canvas = GetComponent<Canvas>();
-            cameraManager = FindObjectsByType<CameraManager>(FindObjectsSortMode.None)[0];
-        }
-
-        void Update()
-        {
-            if(canvas == null || cameraManager == null) return;
-            for(int i=0; i<robots.Length; i++)
-            {
-                Vector2 robotPosInCanvas = WorldToCanvasPosition(canvas, cameraManager.currentCam, baseLinks[i].transform.position);
-                robotMarkers[i].GetComponent<RectTransform>().anchoredPosition = robotPosInCanvas;
-            }
+            
         }
 
     }
