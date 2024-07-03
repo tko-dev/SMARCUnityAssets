0d52542fbe9c6526a9539881c0d3f179cac2ec0f
Ozer Ozkahraman <ozero@kth.se>
2024-07-03 17:53:28 +0200

    First spherecasts

--- Runtime/Scripts/VehicleComponents/Acoustics/Transceiver.cs
+++ Runtime/Scripts/VehicleComponents/Acoustics/Transceiver.cs
@@ -10,6 +10,7 @@ namespace Acoustics
     {   
         public double SoundVelocity = 1500;
         public float MaxRange = 100;
+        public float sphereRadius = 10;
 
         Transceiver[] allTransceivers;
 
@@ -28,16 +29,28 @@ namespace Acoustics
             allTransceivers = GameObject.FindObjectsByType<Transceiver>(FindObjectsSortMode.None);
         }
         
-        void Submit()
+        void Send()
         {
             double now = Clock.NowTimeInSeconds;
             foreach(Transceiver tc in allTransceivers)
             {
                 var id = tc.GetInstanceID();
-                if(id == this.GetInstanceID()) continue;
-                
-                Debug.Log($"Ping other tc:{id}");
-                tc.Receive($"Ping from {this.GetInstanceID()}");
+                if(id == this.GetInstanceID()) continue; // skip self
+
+                Vector3 selfPos = transform.position;
+                Vector3 otherPos = tc.transform.position;
+
+                if((otherPos-selfPos).magnitude > MaxRange) continue; // skip too far
+
+                RaycastHit hit;
+                if(Physics.SphereCast(selfPos, sphereRadius, otherPos-selfPos, out hit, MaxRange))
+                {
+                    Debug.DrawLine(selfPos, hit.point, Color.white, 1);
+                    var dist = hit.distance-sphereRadius;
+                    var c = Color.green;
+                    if(dist < 0) c = Color.red;
+                    Debug.DrawRay(selfPos, dist*((otherPos-selfPos).normalized), c, 1);
+                }            
             }
         }
 
@@ -50,7 +63,7 @@ namespace Acoustics
         {
             if(work)
             {
-                Submit();
+                Send();
             }
         }
 
