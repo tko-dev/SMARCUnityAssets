9911c4c91803113a6ca18fd1e6e9853bd3216c2f
Ozer Ozkahraman <ozero@kth.se>
2024-07-31 17:36:36 +0200

    Drone working nice now?

--- Runtime/Scripts/GameUI/DroneKeyboardController.cs
+++ Runtime/Scripts/GameUI/DroneKeyboardController.cs
@@ -15,8 +15,8 @@ namespace GameUI
 
         Propeller frontrightProp, frontleftProp, backrightProp, backleftProp;
 
-        public float rollRpms = 0.1f;
-        public float moveRpms = 50f;
+        [Tooltip("Difference in RPM between sides of the drone when moving around")]
+        public float RPMDifferenceRatio = 0.25f;
 
         void Awake()
         {
@@ -31,45 +31,53 @@ namespace GameUI
 
             if (Input.GetKeyDown("i"))
             {
-                backrightProp.SetRpm(backrightProp.DefaultHoverRPM * 1.05);
-                backleftProp.SetRpm(backleftProp.DefaultHoverRPM * 1.05);
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM * (1+RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM * (1+RPMDifferenceRatio));
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM * (1-RPMDifferenceRatio));
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM * (1-RPMDifferenceRatio));
             }
 
             if (Input.GetKeyDown("k"))
             {
-                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM * 1.05);
-                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM * 1.05);
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM * (1+RPMDifferenceRatio));
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM * (1+RPMDifferenceRatio));
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM * (1-RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM * (1-RPMDifferenceRatio));
             }
 
             if (Input.GetKeyDown("j"))
             {
-                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM *1.05);
-                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*1.05);
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM *(1+RPMDifferenceRatio));
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM *(1-RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
             }
 
             if (Input.GetKeyDown("l"))
             {
-                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM *1.05);
-                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*1.05);
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM *(1+RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM *(1-RPMDifferenceRatio));
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
             }
 
-            if (Input.GetKeyDown("o"))
+            if (Input.GetKeyDown("u"))
             {
-                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM*1.05);
-                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*1.05);
-                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM*1.05);
-                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*1.05);
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*(1+RPMDifferenceRatio));
             }
 
-            if (Input.GetKeyDown("p"))
+            if (Input.GetKeyDown("n"))
             {
-                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM/1.05);
-                backrightProp.SetRpm(backrightProp.DefaultHoverRPM/1.05);
-                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM/1.05);
-                backleftProp.SetRpm(backleftProp.DefaultHoverRPM/1.05);
+                frontrightProp.SetRpm(frontrightProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
+                backrightProp.SetRpm(backrightProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
+                frontleftProp.SetRpm(frontleftProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
+                backleftProp.SetRpm(backleftProp.DefaultHoverRPM*(1-RPMDifferenceRatio));
             }
             
-            if (Input.GetKeyUp("i") || Input.GetKeyUp("j") || Input.GetKeyUp("k") || Input.GetKeyUp("l") || Input.GetKeyUp("o") || Input.GetKeyUp("p"))
+            if (Input.GetKeyUp("i") || Input.GetKeyUp("j") || Input.GetKeyUp("k") || Input.GetKeyUp("l") || Input.GetKeyUp("u") || Input.GetKeyUp("n"))
             {
                 backrightProp.SetRpm(backrightProp.DefaultHoverRPM);
                 backleftProp.SetRpm(backleftProp.DefaultHoverRPM);
