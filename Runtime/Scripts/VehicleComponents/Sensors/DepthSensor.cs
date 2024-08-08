e80003abbbded979874b10226be396f6b03ca03c
Schni2zle <dolas.aryan312@gmail.com>
2024-08-08 17:19:38 +0200

    depthsensor+datageneration

--- Runtime/Scripts/VehicleComponents/Sensors/DepthSensor.cs
+++ Runtime/Scripts/VehicleComponents/Sensors/DepthSensor.cs
@@ -17,23 +17,65 @@ namespace VehicleComponents.Sensors
         private WaterQueryModel _waterModel;
         private string filePath;
         private bool headerWritten = false;
+        public ArticulationBody droneBody;
+        public float numDatapoints = 10000f;
+        public float dataStep = 100f;
+        public GameObject gameobject;
+
 
         void Start()
         {
             _waterModel = FindObjectsByType<WaterQueryModel>(FindObjectsSortMode.None)[0];
             filePath = Application.dataPath + "/depth_data.csv";
+            
+            droneBody = gameObject.GetComponent<ArticulationBody>();     
         }
 
         public override bool UpdateSensor(double deltaTime)
         {
+
             if (storeDepth)
             {
+                System.Random RNG = new System.Random();
                 float currentTime = Time.time;
+                float i = currentTime/(float)deltaTime;
+                if( i < numDatapoints)
+                {
+                    if (i % dataStep == 0)
+                    {
+                        // Get a random direction between 1 and 4
+                        int direction = RNG.Next(1, 5);
+
+                        // Move the object based on the random direction
+                        switch (direction)
+                        {
+                            case 1:
+                                transform.position += new Vector3(0.5f, 0, 0);  // Increase x by 0.5
+                                break;
+                            case 2:
+                                transform.position += new Vector3(-0.5f, 0, 0);  // Decrease x by 0.5
+                                break;
+                            case 3:
+                                transform.position += new Vector3(0, 0, 0.5f);  // Increase z by 0.5
+                                break;
+                            case 4:
+                                transform.position += new Vector3(0, 0, -0.5f);  // Decrease z by 0.5
+                                break;
+                            default:
+                                Debug.LogWarning("Unexpected direction value: " + direction);
+                                break;
+                        }
+                    }
+                }
+                else{storeDepth = false;}
+            
+                
                 var waterSurfaceLevel = _waterModel.GetWaterLevelAt(transform.position);
                 float depth = transform.position.y - waterSurfaceLevel;
 
                 // Store x, y, z, time, and depth in CSV
                 AppendDataToCSV(transform.position.x, transform.position.y, transform.position.z, currentTime, depth);
+                
                 return true; // Successfully stored data
             }
 
