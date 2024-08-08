542e938d957595b8231c36a49d5652c7cf2c2883
Schni2zle <dolas.aryan312@gmail.com>
2024-08-08 11:39:49 +0200

    fixed depthsensor

--- Runtime/Scripts/VehicleComponents/Sensors/DepthSensor.cs
+++ Runtime/Scripts/VehicleComponents/Sensors/DepthSensor.cs
@@ -34,7 +34,10 @@ namespace VehicleComponents.Sensors
 
                 // Store x, y, z, time, and depth in CSV
                 AppendDataToCSV(transform.position.x, transform.position.y, transform.position.z, currentTime, depth);
+                return true; // Successfully stored data
             }
+
+            return false; // Depth data was not stored
         }
 
         private void AppendDataToCSV(float x, float y, float z, float time, float depth)
