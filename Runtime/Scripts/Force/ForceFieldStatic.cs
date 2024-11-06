95c28aa22b98b5676a662020b043a86d9e42e686
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2024-11-06 15:49:52 +0100

    Repulsive and Attractive static field

--- Runtime/Scripts/Force/ForceFieldStatic.cs
+++ Runtime/Scripts/Force/ForceFieldStatic.cs
@@ -2,16 +2,39 @@ using UnityEngine;
 
 namespace Force
 {
+    public enum StaticForceFieldMode
+    {
+        Vector,
+        Attarction,
+        Repulsion
+    }
+
     public class ForceFieldStatic : ForceFieldBase
     {
 
         [Header("Static Force Field")]
-        [Tooltip("The force that will be applied to all ForcePoints in this field.")]
-        public Vector3 staticForce;
+        public StaticForceFieldMode mode = StaticForceFieldMode.Vector;
+
+        [Tooltip("Magnitude will be over-written by ForceMagnitude.")]
+        public Vector3 ForceVector;
+        public float ForceMagnitude = 1f;
 
         protected override Vector3 Field(Vector3 position)
         {
-            return staticForce;
+            switch(mode)
+            {
+                case StaticForceFieldMode.Vector:
+                    return ForceVector.normalized * ForceMagnitude;
+
+                case StaticForceFieldMode.Repulsion:
+                    return (position - transform.position).normalized * ForceMagnitude;
+
+                case StaticForceFieldMode.Attarction:
+                    return (transform.position - position).normalized * ForceMagnitude;
+
+                default:
+                    return Vector3.zero;
+            }
         }
     }
 }
